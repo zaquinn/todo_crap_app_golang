5dd8a01d8c47cb6987d07c518c8e15a5eb67f9b9
zaquinn <ayzick120@gmail.com>
2025-01-29 18:43:22 -0300

    feat: add graceful shutdown

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
 	"crud/todo-crap-app/internal/todo"
 	"crud/todo-crap-app/pkg/middleware"
 	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
@@ -17,7 +22,7 @@ import (
 func main() {
 	db, err := sql.Open("postgres", "postgres://root:root@localhost:5432/todo_crap_app?sslmode=disable")
 	if err != nil {
-		panic(err)
+		log.Fatalf("Could not connect to database: %v", err)
 	}
 	defer db.Close()
 
@@ -26,13 +31,35 @@ func main() {
 	}
 
 	mux := http.NewServeMux()
-
 	todoRepository := todo.NewRepository(db)
-
 	todo.RegisterRoutes(mux, todoRepository)
 
-	fmt.Println("Starting server on http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", middleware.LoggingMiddleware(middleware.CorsMiddleware(mux))))
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: middleware.LoggingMiddleware(middleware.CorsMiddleware(mux)),
+	}
+
+	go func() {
+		log.Printf("Starting server on http://localhost:8080")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Could not start server: %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v", err)
+	}
+
+	log.Println("Server exited gracefully")
 }
 
 func runMigrations(db *sql.DB) error {
